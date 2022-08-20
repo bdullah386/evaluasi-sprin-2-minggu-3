e0760f313d4d59355cae9c56f6acdd42221b5836
bdullah386 <bdullahbdullah386@gmail.com>
2022-08-20 10:48:26 +0700

    evaluasi

---
+++ main.js
@@ -0,0 +1,125 @@
+// function Romawi(id){
+//     switch(id){
+//         case 1:
+//             return  "I";
+//             break;
+//          case 2:
+//                 return  "II";
+//                 break;
+//          case 3:
+//                 return  "III";
+//                 break;
+//          case 4:
+//                 return  "IV";
+//                 break;
+//           case 5:
+//                     return  "V";
+//                     break;
+//             case 6:
+//                     return  "VI";
+//                      break;
+
+//              case 7:
+//                     return  "VII";
+//                     break;
+//             case 8:
+//                         return  "VIII";
+//                         break;
+//              case 9:
+//                         return  "IX";
+//                          break;
+//             case 10:
+//                 return "X"
+//                 break;
+//     }
+// }
+
+
+
+
+
+// function negativepositive(){
+//     return arr
+//     let negative=[]
+//     let positive =[]
+//     for(let index in negativepositive){
+//         if(negative %2 == 0){
+//             console.log(negative[index]);
+//         }else{
+//             console.log(positive[index]);
+//         }
+//     }
+//     arguments(([1,2,3,4,-5,-6,-7,-8]))
+// }
+
+
+
+// function rataRata({    aqidah : "100", matematika : "75.5", fisika : "87",  fisika : "87",    arab : "90.8",  inggris: "80"};)
+//  {
+//     let total = []
+    
+//     // for(let index in rataRata){
+//     //     return rataRata 
+//     // }
+//     total.unshift(rataRata[index])
+//     for(let index in rataRata){
+//         if(total == rataRata[index]){
+//             console.log(87,22);
+//         }
+//         return rataRata
+//     }
+// }
+
+
+
+//'buku I'
+// ['codingan dasar'],
+//"iz",
+// false
+// let todo = []
+
+// function addtodo(id,title,genre,author,isBorrow){
+//  let books =    [
+//         {
+//             id: +newDate,
+//             title: 
+//             genre:
+//             author:
+//             isBorrow:
+//         }
+//     ]
+// }
+
+// showAlltodo(){
+//     return showAlltodo
+// }
+
+
+
+let books = [
+    // {
+    //         id: 1,
+    //         title:'kalkulus jilid I',
+    //         genre:['matematika','bahasa indonesia'],
+    //         author:'jun',
+    //         isBorrow:false
+    // }
+]
+
+function addbooks(id,title,genre,author,isBorrow){
+    id: +newDate
+    title;
+    genre;
+    author;
+    isBorrow;
+}
+
+
+function showAllbooks(){
+    return showAllbooks
+}
+
+function updatebooks(){
+
+}
+
